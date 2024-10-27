764d4986f723a7eb2fce39ad057029273b059a8d
Park JeaHyun <jea5158@gmail.com>
2024-10-27 15:24:52 +0900

    형변환

---
+++ src/casting/Casting2.java
@@ -0,0 +1,11 @@
+package casting;
+
+public class Casting2 {
+    public static void main(String[] args) {
+        double dv = 1.5;
+        int iv = 0;
+
+        iv = (int) dv;
+        System.out.println(iv);
+    }
+}
---
+++ src/casting/Casting3.java
@@ -0,0 +1,8 @@
+package casting;
+
+public class Casting3 {
+    public static void main(String[] args) {
+        System.out.println(1 + 'A');
+        System.out.println((char) (1 + 'A'));
+    }
+}
